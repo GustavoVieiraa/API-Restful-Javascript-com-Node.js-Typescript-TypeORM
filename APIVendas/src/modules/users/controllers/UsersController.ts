249e8aff1e4658c78fd0f8be21218972e80f88f2
Gustavo Vieira <gustavieiradev@gmail.com>
2024-10-16 08:04:43 -0300

    feat: UsersController created for handling user services.

---
+++ APIVendas/src/modules/users/controllers/UsersController.ts
@@ -0,0 +1,32 @@
+import { Request, response, Response } from 'express'
+import ListUserService from '../services/ListUserService'
+import CreateUserService from '../services/CreateUserService';
+
+export default class UsersController {
+  public async index(request: Request, response: Response): Promise<Response> {
+
+    const listUser = new ListUserService();
+
+    const users = await listUser.execute()
+
+    return response.json(users).status(200);
+
+  }
+
+  public async create(request: Request, response: Response): Promise<Response> {
+
+    const { name, email, password } = request.body;
+
+    const createUser = new CreateUserService();
+
+    const user = await createUser.execute({
+      name,
+      email,
+      password
+    })
+
+    return response.json(user).status(201);
+
+  }
+
+}
