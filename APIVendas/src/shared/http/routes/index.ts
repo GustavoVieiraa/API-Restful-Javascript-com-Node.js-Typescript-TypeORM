94351de3e16e9c5174b93a9a975c2a1ee813000c
Gustavo Vieira <gustavieiradev@gmail.com>
2024-10-16 09:24:14 -0300

    feat: implementing users routes.

--- APIVendas/src/shared/http/routes/index.ts
+++ APIVendas/src/shared/http/routes/index.ts
@@ -1,8 +1,10 @@
 import productsRouter from '@config/products/routes/products.routes';
+import usersRouter from '@config/users/routes/users.routes';
 import { Router } from 'express';
 
 const routes = Router();
 
 routes.use('/products', productsRouter);
+routes.use('/users', usersRouter);
 
 export default routes;
