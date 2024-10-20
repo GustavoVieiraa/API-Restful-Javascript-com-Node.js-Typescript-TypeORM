11438e22bf5d0e85d7b0223496db282bac51c8e4
Gustavo Vieira <gustavieiradev@gmail.com>
2024-10-19 23:18:37 -0300

    feat: EtherealMail implementation and using in the SendForgot for recuperation password.

--- APIVendas/src/modules/users/services/SendForgotPasswordEmailService.ts
+++ APIVendas/src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -1,6 +1,7 @@
 import { getCustomRepository } from "typeorm";
 import UsersRepository from "../typeorm/repositories/UsersRepository";
 import UserTokensRepository from "../typeorm/repositories/UserTokensRepository";
+import EtherealMail from "@config/mail/EtherealMail";
 import AppError from "@shared/errors/AppError";
 
 
@@ -22,7 +23,10 @@ class SendForgotPasswordEmailService {
 
     const token = await userTokenRepository.generate(user.id);
 
-    console.log(token);
+    await EtherealMail.sendMail({
+      to: email,
+      body: `Solicitação de redefinição de senha recebida: ${token?.token}`,
+    });
 
   }
 }
