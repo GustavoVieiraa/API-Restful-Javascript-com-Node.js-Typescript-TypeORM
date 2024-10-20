7fb50c1daaeea1f6743cea17c61ae9a124f4fd49
Gustavo Vieira <gustavieiradev@gmail.com>
2024-10-19 22:34:19 -0300

    feat: added password routes

--- APIVendas/src/shared/http/routes/index.ts
+++ APIVendas/src/shared/http/routes/index.ts
@@ -1,4 +1,5 @@
 import productsRouter from '@modules/products/routes/products.routes';
+import passwordRouter from '@modules/users/routes/password.routes';
 import sessionsRouter from '@modules/users/routes/sessions.routes';
 import usersRouter from '@modules/users/routes/users.routes';
 import { Router } from 'express';
@@ -8,5 +9,6 @@ const routes = Router();
 routes.use('/api/v1/products', productsRouter);
 routes.use('/api/v1/users', usersRouter);
 routes.use('/api/v1/sessions', sessionsRouter);
+routes.use('/api/v1/password', passwordRouter);
 
 export default routes;
