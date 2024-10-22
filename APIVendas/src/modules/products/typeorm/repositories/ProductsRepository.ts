26f8a25763d44a6d18a99a31e0c26d81355ebed4
Gustavo Vieira <gustavieiradev@gmail.com>
2024-10-22 12:23:12 -0300

    feat: added CreateOrderService for creations our orders in the system.

--- APIVendas/src/modules/products/typeorm/repositories/ProductsRepository.ts
+++ APIVendas/src/modules/products/typeorm/repositories/ProductsRepository.ts
@@ -1,19 +1,34 @@
-import {EntityRepository, Repository} from 'typeorm';
+import {EntityRepository, Repository, In} from 'typeorm';
 import Product from '../entities/Product';
+
+interface IFindProducts {
+  id: string;
+}
 
 @EntityRepository(Product)
 export class ProductRepository extends Repository<Product> {
 
   public async findByName(name: string): Promise<Product | undefined> {
-
     const product = await this.findOne({
       where: {
         name,
       }
     });
-
     return product
-
   }
+
+  public async findAllByIds(products: IFindProducts[]): Promise<Product[]> {
+      
+    const productsIds = products.map(products => products.id);
+
+    const existsProducts = await this.find({
+      where: {
+        id: In(productsIds),
+      }
+    })
+
+    return existsProducts;
+
+  } 
  
 }
